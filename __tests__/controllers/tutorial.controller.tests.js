38ac0e723ada9a995c504d5481591fe61615a601
emilypie <empielemeier@gmail.com>
2022-06-21 17:58:15 -0500

    controller tests for findAll

---
+++ __tests__/controllers/tutorial.controller.tests.js
@@ -0,0 +1,68 @@
+// mock out Sequelize
+const sequelize = jest.mock('sequelize');
+//mock out database model
+const db = require('../../app/models');
+db.tutorials = {};
+
+const app = require('../../server.js');
+const request = require('supertest');
+
+describe('tutorials controller', () => {
+
+  var testTutorial = {
+    title: "Automated Testing Tutorial",
+    description: "This tutorial shows an example test suite of a NodeJS backend",
+    published: false
+  };
+
+  describe('get tutorials list', () => {
+    it('calls findAll without query', async () => {
+      db.tutorials.findAll = jest.fn().mockResolvedValue(Promise.resolve([]));
+      await request(app)
+        .get('/api/tutorials')
+        .expect(200)
+        .then((response) => {
+          expect(db.tutorials.findAll).toHaveBeenCalled();
+        });
+    });
+
+    it('calls findAll with query', async () => {
+      db.tutorials.findAll = jest.fn().mockResolvedValue(Promise.resolve([]));
+      await request(app)
+        .get('/api/tutorials?title=Automated')
+        .expect(200)
+        .then((response) => {
+          expect(db.tutorials.findAll).toHaveBeenCalledWith({
+            where: {
+              title: {
+                [db.Sequelize.Op.like]: "%Automated%"
+              }
+            }
+          });
+        });
+    });
+
+    it('responds with results from findAll', async () => {
+      db.tutorials.findAll = jest.fn().mockResolvedValue(
+        Promise.resolve([testTutorial]));
+      await request(app)
+        .get('/api/tutorials')
+        .expect(200)
+        .then((response) => {
+          expect(response.body).toHaveLength(1);
+          expect(response.body[0]).toMatchObject(testTutorial);
+        });
+    });
+
+    it('responds with 500 and message on error', async () => {
+      db.tutorials.findAll = jest.fn().mockImplementation(() =>
+        Promise.reject(new Error("Fake error from test")));
+      await request(app)
+        .get('/api/tutorials')
+        .expect(500)
+        .then((response) => {
+          expect(response.body.message).toBe("Fake error from test");
+        });
+    });
+  });
+});
